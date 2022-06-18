faba03926a5ccf99e26f1c21f8b631c1510fa8da
Hernán Rodríguez <hsrodriguez@outlook.com.ar>
2022-06-17 22:30:29 -0300

    Added functionality to 'Disable similar characters'.

--- scripts/passwordGenerator.js
+++ scripts/passwordGenerator.js
@@ -5,6 +5,7 @@ const numbersList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const lowercaseList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const uppercaseList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const symbolsList = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+const similarList = [1, 'i', 'I', 'j', 'l', '!', '|', 'o', 'O', 0];
 
 function generatePassword() {
     const passwordLength = document.getElementById("passwordLength").value;
@@ -24,18 +25,8 @@ function generatePassword() {
     let seedArray = [];
 
     // Add each selected list to the password seed
-    if(document.getElementById("useNumbers").checked) {
-        seedArray = seedArray.concat(numbersList);
-    }
-    if (document.getElementById("useLowercase").checked) {
-        seedArray = seedArray.concat(lowercaseList);
-    }
-    if (document.getElementById("useUppercase").checked) {
-        seedArray = seedArray.concat(uppercaseList);
-    }
-    if (document.getElementById("useSymbols").checked) {
-        seedArray = seedArray.concat(symbolsList);
-    }
+    seedArray = generateSeedArray(seedArray);
+    console.log(seedArray);
 
     // If no character sets are selected, exit 
     if (seedArray == "") {
@@ -51,6 +42,28 @@ function generatePassword() {
     }
 
     document.getElementById("generatedPassword").value = generatedPassword;
+}
+
+function generateSeedArray() {
+    let arr = [];
+    if (document.getElementById("useNumbers").checked) {
+        arr = arr.concat(numbersList);
+    }
+    if (document.getElementById("useLowercase").checked) {
+        arr = arr.concat(lowercaseList);
+    }
+    if (document.getElementById("useUppercase").checked) {
+        arr = arr.concat(uppercaseList);
+    }
+    if (document.getElementById("useSymbols").checked) {
+        arr = arr.concat(symbolsList);
+    }
+    if (document.getElementById("disableSimilar").checked) {
+        arr = arr.filter(function findSimilar (item){
+            return !similarList.includes(item);
+        });
+    }
+    return arr;
 }
 
 function clipboardCopy() {
